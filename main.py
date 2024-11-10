1212e4f398ec7d125576b41941e208d0fe442171
Vivian Leigh <viviandach9@gmail.com>
2024-11-10 02:03:17 +0200

    I yoinked this out at some point.

--- main.py
+++ main.py
@@ -77,6 +77,7 @@ class AdminBot(commands.Bot):
         self.logger.info(f'Connected to {len(self.guilds)} guilds')
         
         await self.change_presence(
+            status=discord.Status.dnd,
             activity=discord.Activity(
                 type=discord.ActivityType.watching,
                 name="you through ur walls"
